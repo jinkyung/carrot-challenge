1343f4d9e739b6da33d5027ffe921a286ad7ffc7
jinkyung jeong <jinkyungj@gmail.com>
2024-08-30 17:15:13 +0900

    rounded 추가

--- components/tweet.tsx
+++ components/tweet.tsx
@@ -8,7 +8,7 @@ export default function Tweet({
   created_at: Date;
 }) {
   return (
-    <div className="bg-neutral-100 w-full p-3 flex flex-col gap-3">
+    <div className="bg-neutral-100 w-full p-3 flex flex-col gap-3 rounded-md">
       <div className="font-medium">{tweet}</div>
       <div className="text-gray-400 text-sm flex gap-1.5 justify-end">
         <span>Written</span>
